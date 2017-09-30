ce394786a6318be7ec20553d310df65c9747da67
mkery <mkery@cs.cmu.edu>
2017-09-30 15:28:24 -0400

    updated for walk task

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SSUI-KittyCatJS
 Some starter code for a Software Structures for User Interfaces (CMU course Fall 2017) lab to teach event handling and other features in Javascript. With cats! ^ↀᴥↀ^
+
+
+Image Resource Attributions:
+Moon tile adapted from open game art resource by rrcaseyr
+Cat pixel art: unkown artist
---
+++ js/cat.js
@@ -0,0 +1,60 @@
+"use strict";
+
+var groundY = -80;
+var stepX = 5;
+var stepY = 10;
+
+document.addEventListener("keyup", function(ev){
+	if (ev.keyCode === 32) //space key
+	{
+      jump();
+    }
+});
+
+function jump(up = true, ease = 5)
+{
+	var kat = document.getElementById('katnis');
+	var top = parseInt(kat.style.top) || groundY;
+	
+	if(top > -180 && up) {
+		goUp();
+		goForward();
+		setTimeout(function() {jump(true, ease/0.9);}, ease);
+	}
+	else if(top <= groundY) {
+		goDown();
+		goForward();
+		setTimeout(function() {jump(false, ease/1.5);}, ease);
+	}
+	
+}
+
+function goForward()
+{
+	if(canGoForward()) {
+		var kat = document.getElementById('katnis');
+		var left = parseInt(kat.style.left) || 0;
+		kat.style.left = left + stepX + "px";	
+	}
+}
+
+function goUp()
+{
+	var kat = document.getElementById('katnis');
+	var top = parseInt(kat.style.top) || groundY;
+	kat.style.top = top - stepY + "px";
+}
+
+function goDown()
+{
+	var kat = document.getElementById('katnis');
+	var top = parseInt(kat.style.top) || groundY;
+	kat.style.top = top + stepY + "px";
+}
+
+function canGoForward()
+{
+	var kat = document.getElementById('katnis');
+	var right = (parseInt(kat.style.left) || 0) + parseInt(kat.width);
+	return right + stepX < window.innerWidth;
+}
